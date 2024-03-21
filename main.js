b452f6ac256750b54284ed1321b34bdc432a822c
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-21 23:01:52 +0800

    testremove P

--- main.js
+++ main.js
@@ -6,37 +6,25 @@ window.SpeechRecognition =
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-let p = document.createElement("p");
-
 recognition.addEventListener("result", (e) => {
-  texts.appendChild(p);
   const text = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
     .join("");
 
-  p.innerText = text;
+  console.log(text)
   if (e.results[0].isFinal) {
     if (text.includes("how are you")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "I am fine";
-      texts.appendChild(p);
+      console.log("Okay")
     }
     if (
       text.includes("what's your name") ||
       text.includes("what is your name")
     ) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "My Name is Cifar";
-      texts.appendChild(p);
+      console.log("Nigga")
     }
     if (text.includes("open my YouTube")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "opening youtube channel";
-      texts.appendChild(p);
+      console.log("Okay")
       console.log("opening youtube");
       window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
     }
