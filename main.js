dda6ec002032bb6bcb1f245369a9d70d6319927a
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-21 22:53:39 +0800

    update script

--- main.js
+++ main.js
@@ -6,25 +6,22 @@ window.SpeechRecognition =
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-let p = document.querySelector(".texts p");
-
-const commands = [];
+let p = document.createElement("p");
 
 recognition.addEventListener("result", (e) => {
-  let p = document.querySelector(".texts p");
+  texts.appendChild(p);
   const text = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
-    .join("").toLowerCase();
-    console.log(text);
-    p.innerText = text;
+    .join("");
+
+  p.innerText = text;
   if (e.results[0].isFinal) {
-    console.log("Aba Ewan",text);
-    p.innerText = text;
-    commands.push(text);
-    if (text.includes("give me the array")) {
-        console.log(commands)
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
       p.innerText = "I am fine";
+      texts.appendChild(p);
     }
     if (
       text.includes("what's your name") ||
@@ -35,13 +32,13 @@ recognition.addEventListener("result", (e) => {
       p.innerText = "My Name is Cifar";
       texts.appendChild(p);
     }
-    if (text.includes("open my youtube")) {
+    if (text.includes("open my YouTube")) {
       p = document.createElement("p");
       p.classList.add("replay");
       p.innerText = "opening youtube channel";
       texts.appendChild(p);
       console.log("opening youtube");
-      window.open("https://www.youtube.com");
+      window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
     }
     p = document.createElement("p");
   }
