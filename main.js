0b6d8f84166c183a56f6b8be6548d5f4bd76012d
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-26 10:35:18 +0800

    update script

--- main.js
+++ main.js
@@ -23,9 +23,11 @@ recognition.addEventListener("result", (e) => {
     .join("");
 
   console.log(text);
+  disp.style.border = '2px solid lawngreen'
   status.innerText = "Listening..."
   disp.innerText = text;
   if (e.results[0].isFinal) {
+    disp.style.border = 'none';
     status.innerText = "Say Something...";
     disp.innerText = text;
     chache.push(text);
@@ -72,24 +74,18 @@ function command(text){
 function searchCMD(text){
   function extractSearchQuery(input) {
     // Regular expression to match the search query
-    const regex = /search\s+(.*?)\s+on\s+/i; // Matches "search" followed by one or more characters
+    const regex = /search\s+(.+)/i; // Matches "search" followed by one or more characters
     // Execute the regular expression on the input string
     const match = input.match(regex);
 
     let searchQuery;
 
   
-    if (match && match.length > 1 && input.includes("on new tab")) {
+    if (match && match.length > 1) {
       // Extract the search query from the matched groups
       searchQuery = match[1].trim(); // Extracted search query
 
-      window.open(`https://www.google.com/search?q=${searchQuery}&rlz=1C1JZAP_enUS994US995&oq=izanamii&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgkIARAuGAoYgAQyCQgCEAAYChiABDIJCAMQLhgKGIAEMgkIBBAAGAoYgAQyCQgFEAAYChiABDIJCAYQABgKGIAEMgwIBxAuGAoY1AIYgAQyDAgIEC4YChjUAhiABNIBCDM1MDBqMGo5qAIAsAIA&sourceid=chrome&ie=UTF-8#cobssid=s`);
-    } else {
-      window.location.href = "#web"
-    }
-    if(match && match.length > 1 && input.includes("on new tab ang go to image")){
-      searchQuery = match[1].trim();
-      window.open(`https://www.google.com/search?sca_esv=ff48bec18c46a1e3&rlz=1C1JZAP_enUS994US995&sxsrf=ACQVn0-9CspMToKIdkT8gazL7nx0EIknbQ:1711107050501&q=${searchQuery}&uds=AMwkrPsrDfxfgTr36wdxCAEhRxQNB5MQLH7fYdE9QiqXnGjv3-FFMq76dgvyEag4ac3AONMMS6MrBfOBNHpDK9HelGYWeBJIb66T-J9gYEvleerGlBABHmJADacacywv2xH6M82V4CSt1UPSmlPSk9gqTv1RHyehVNT1RQWGrQvmuxTH2_r6q_dF4U3NITZxNhuBeIXArt-BxR4xlyFABaNoiDjGZ6MBZGZtpMYOcmSi9FqEL3AA2GQvRV35MQZpGDOa2HidZFA2CwYKisRco2y4YzHPRwmNaVOncnHD-bRA2RwJzSm5Nqz_-bPnu629CY3XFTgoPK10&udm=2&prmd=vinsmbtz&sa=X&ved=2ahUKEwjzmau_4oeFAxW-Z_UHHeilAmsQtKgLegQIDRAB&biw=1366&bih=679&dpr=1`);
+      window.open(`https://www.google.com/search?q=${searchQuery}&rlz=1C1JZAP_enUS994US995&oq=hello&gs_lcrp=EgZjaHJvbWUqDQgAEAAY4wIYsQMYgAQyDQgAEAAY4wIYsQMYgAQyCggBEC4YsQMYgAQyCggCEC4YsQMYgAQyDQgDEAAYsQMYgAQYigUyCggEEC4YsQMYgAQyBggFEEUYPTIGCAYQRRg8MgYIBxBFGDzSAQg1MzYxajBqNKgCALACAA&sourceid=chrome&ie=UTF-8`);
     }
   }
   extractSearchQuery(text)
